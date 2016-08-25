a75e85765c36da08f7a37734dc0f36e3f4ba4822
AMRESH KADIAN <askadian@users.noreply.github.com>
2016-08-25 09:46:06 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -7,6 +7,10 @@ config(['$routeProvider',function($routeProvider) {
             templateUrl: 'partials/home.tpl.html',
             controller: 'HomeCtrl'
         })
+        .when('/Login', {
+            templateUrl: 'partials/login.tpl.html',
+            controller: 'HomeCtrl'
+        })
         .when('/Contact', {
             templateUrl: 'partials/contact.tpl.html',
             controller: 'HomeCtrl'
@@ -22,7 +26,9 @@ appModule.controller('HeaderCtrl', ['$location', '$rootScope', '$scope', functio
     $rootScope.curLoc = $location.path();
     $rootScope.header = {
         bottom : false,
-        categories: ['all', 'hindi', 'telugu', 'english', 'tamil', 'malyalam', 'kannada', 'bengali', 'assami', 'kids', 'animation', 'games']
+        categories: ['all', 'hindi', 'telugu', 'english', 
+        'tamil', 'malyalam', 'kannada', 'bengali', 'assami', 
+        'kids', 'animation', 'games']
     };
     console.log($rootScope.curLoc + ": In Header Controller.");
     $rootScope.$on('changeHeader', function (event, params) {
@@ -46,39 +52,21 @@ appModule.controller('HomeCtrl', ['$location', '$rootScope', '$scope', '$firebas
         $rootScope.header.bottom = false;
     }
 
-
     $scope.data = $firebaseArray(ref);
 
-
-    var name = "AMRESH KADIAN";
-    $rootScope.$broadcast('changeHeader', {key: $scope.data.name});
+    $rootScope.$broadcast('changeHeader', {key: "abc"});
 }]);
 
-appModule.controller('PreviewCtrl', ['$scope', '$routeParams', '$firebaseObject', function($scope, $routeParams, $firebaseObject) {
-    $scope.model = $routeParams.mdl;
+appModule.controller('PreviewCtrl', ['$rootScope', '$scope', '$routeParams', '$firebaseObject', function($rootScope, $scope, $routeParams, $firebaseObject) {
+    $rootScope.header.bottom = false;
+    $rootScope.$broadcast('changeHeader', {key: "abc"});
+    $scope.model = +$routeParams.mdl;
     console.log($scope.model + ": In Preview Controller.");
     var ref = new Firebase('https://mystore-f6c12.firebaseio.com/products/');
-    // $scope.previewProduct = firebase.database().ref('user-posts/' + myUserId)
-
-    // new Firebase("https://mystore-f6c12.firebaseio.com/products/")
-    // .startAt(1)
-    // .endAt(1)
-    // .once('value', function(snap) {
-    //    console.log('accounts matching email address', snap.val())
-    // });
-
-   // ref.orderByChild("mdl").equalTo($scope.model).on("child_added", function(snapshot) {
-    ref.once('value', function(snapshot) {
-        var productsData = snapshot.val()
-       
-        let productId = productsData.findIndex(function(product) {
-            console.log('Scope model ', $scope.model)
-            return product.mdl === $scope.model
-        })
-
-        console.log('Inside the firebase event handler ', productId)
-        $scope.previewProduct = productId;
-        console.log($scope.previewProduct + ": In Preview Controller.");
+    ref.orderByChild("mdl").startAt($scope.model).endAt($scope.model).on("child_added", function(snapshot) {
+      console.log(snapshot.key() + " was " + snapshot.val().mdl + " meters tall");
+      $scope.previewProduct = snapshot.val();
     });
+    //ref.off();
 }
 ]);
